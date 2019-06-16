9211ae1d80d91738389e097028a2e5361db0e996
alex musa <alex.musa1988@gmail.com>
2019-06-16 15:20:13 -0500

    --- Auto-Git Commit ---

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+var LettersArray = ["a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "x",
+    "y",
+    "z"
+];
+
+var wins = 0;
+var losses = 0;
+var left = 9;
+var guesses = 9;
+var guessesSoFar = [];
+var varLetter;
